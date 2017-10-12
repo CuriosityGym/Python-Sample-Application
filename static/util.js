a7a86a0c85aa768e5a3364acde094d956e7090b0
rupin <rupin.chheda@gmail.com>
2017-10-12 18:48:28 +0530

    MEssages

--- static/util.js
+++ static/util.js
@@ -5,3 +5,7 @@ function action(endpoint_name) {
 function redirect_to_demo(code) {
     window.location.replace('/demo');
 }
+
+function redirectToDefault() {
+    window.location.replace('/login');
+}
