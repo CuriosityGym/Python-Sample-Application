7963a0d82a330a5ad0e06c016d15579f3aa3c9a2
rupin <rupin.chheda@gmail.com>
2017-10-12 19:49:19 +0530

    Creds

--- app.py
+++ app.py
@@ -187,8 +187,11 @@ def submit():
     #os.putenv('UBER_REFRESH_TOKEN',response.json().get('refresh_token'))
     #print os.environ.get('UBER_REFRESH_TOKEN')
     #file=open("credentials.json","w").close() #erase all contents of the file
-    with open('credentials.json', 'w') as outfile:
-        json.dump(response.json(), outfile)
+    file = open('credentials.json','w')
+    file.seek(0)                        # <- This is the missing piece
+    file.truncate()  
+    file.write(response.json())
+    file.close()
     return "OK"
 
 
