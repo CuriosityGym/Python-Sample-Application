fe22c3fcafb2054a7d0994bd5b435ebcfce29b31
rupin <rupin.chheda@gmail.com>
2017-10-13 13:20:00 +0530

    Last Modified Time

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def getAccessToken():
     
 def modification_date(filename):
     t = os.path.getmtime(filename)
-    return datetime.datetime.fromtimestamp(t)    
+    return t    
 
 @app.route('/lastModified', methods=['GET'])
 def lastModified():
